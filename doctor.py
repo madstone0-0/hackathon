6c6add342649fae1d9efaaf44ffd464e0ba17c95
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-11-01 22:50:59 +0000

    Lets fuckking goooo

--- doctor.py
+++ doctor.py
@@ -1,3 +1,4 @@
+from time import sleep
 import pygame as pg
 from maze import Grid
 from constants import *
@@ -31,6 +32,27 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        # if doctor.lives == 0 or len(boxes) == 0:
+        #     if event.type == pg.KEYDOWN:
+        #         if event.key == pygame.K_q:
+        #             exit()
+        #         if event.type == pygame.K_r:
+        #             grid = Grid(SCREEN, 30, screen)
+        #             grid.genMazeDFS()
+        #             obs = grid.getObs()
+        #             boxes = grid.boxes
+        #             print("Done getting objects")
+        #             doctor = Doctor(grid=obs)
+        #             doctor.initialize(obs)
+        #             hasBoxesPlaced = False
+        #         else:
+        #             continue
+        # key = pygame.key.get_pressed()
+        # while not (key[pygame.K_q] or key[pygame.K_r]):
+        #     key = pygame.key.get_pressed()
+        # if key[pygame.K_q]:
+        #     exit()
+
     # Update
     # Render
     # Update player position with collision detection
@@ -50,14 +72,19 @@ while running:
     text_x = SCREEN_WIDTH + 10  # 10 pixels from the right edge
     text_y = 10
     screen.blit(lives_text, (text_x, text_y))
-    if doctor.lives == 0:
-        help_text = "Our dear doctor has died. Press q to quit or r"
-        screen.blit(help_text, (text_x, text_y + 10))
-        key = pygame.key.get_pressed()
-        while not (key[pygame.K_q] or key[pygame.K_r]):
-            key = pygame.key.get_pressed()
-        if key[pygame.K_q]:
-            exit()
+
+    if doctor.lives == 0 or len(boxes) == 0:
+        winStr = ""
+        if doctor.lives == 0:
+            winStr = "The doctor died You lost"
+        else:
+            winStr = "Lets get that bread gamers"
+
+        help_text = font.render(winStr, True, WHITE)
+        screen.blit(help_text, (text_x, text_y + 20))
+        pygame.display.flip()
+        sleep(2)
+        exit()
 
         # reset the game
         grid.genMazeDFS()
