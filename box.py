16ee7913a657b5e7990734af8e4773dfabfbe762
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-11-01 22:29:27 +0000

    Merged

--- box.py
+++ box.py
@@ -6,7 +6,8 @@ from main import *
 class Box:
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 20, 20)
-        self.isApple = rng(2) == 1
+        # self.isApple = rng(2) == 1
+        self.isApple = True
         # apple if = 1 else bread
 
     def draw(self, surface):
