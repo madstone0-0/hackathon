dcf5e05a868b3d07aba2f886f28bb0671e893f63
nignig0 <tanitoluwaadebayo@gmail.com>
2024-11-01 20:08:36 +0000

    doctor is kinda done

--- doctor.py
+++ doctor.py
@@ -1,12 +1,17 @@
 import pygame as pg
 from maze import Grid
+from constants import *
+from doctor_player import *
 
 pg.init()
-SCREEN = pg.math.Vector2(1280, 720)
+SCREEN = pg.math.Vector2(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pg.display.set_mode(SCREEN)
 clock = pg.time.Clock()
 running = True
 grid = Grid(SCREEN, 10, screen)
+
+doctor = Doctor()
+doctor.initialize(TEMP_WALLS)
 
 while running:
     # Handle events
@@ -15,10 +20,18 @@ while running:
             running = False
     # Update
     # Render
-    screen.fill("blue")
-    # grid.render()
-    pg.draw.rect(screen, (255, 0, 0), pg.Rect(30, 30, 60, 60))
-    pg.display.flip()
-    clock.tick(60)
+     # Update player position with collision detection
+    doctor.move(TEMP_WALLS)
+
+    # Draw everything
+    screen.fill(BLACK)
+    doctor.draw(screen)
+    for wall in TEMP_WALLS:
+        pygame.draw.rect(screen, RED, wall)
+
+    # Update the display
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
 
 pg.quit()
